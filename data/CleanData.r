2e7f0d777c9a9b78eabb67b81bc938bf639cd67c
sadorrab <ben.a.sadorra@gmail.com>
2024-04-24 14:47:27 -0400

    update clean script

--- data/CleanData.r
+++ data/CleanData.r
@@ -1,6 +1,10 @@
 library("dplyr")
 df_calls <- read.csv("911BehavioralHealthDiversion.csv")
+#Entries are identified by police district, year, and month
+#for each entry we observe call frequency, number of trees, and whether the month is peak fly activity
 df <- df_calls %>%
-	mutate(Year=substring(callDateTime,1,4), #extract month and year from calls
-		Month=substring(callDateTime,6,7))
-write.csv(df,"data.csv")
+	mutate(Year=as.numeric(substring(callDateTime,1,4)),
+		Month=as.numeric(substring(callDateTime,6,7))) %>%
+	group_by(policeDistrict, Year, Month) %>%
+	summarise(callFreq=n())
+write.csv(df,"callFrequency.csv")
