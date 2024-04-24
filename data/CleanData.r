af8299908de3590ab8f202d499667c4c1a8b6059
sadorrab <ben.a.sadorra@gmail.com>
2024-04-24 16:13:08 -0400

    fixed column order

--- data/CleanData.r
+++ data/CleanData.r
@@ -7,9 +7,9 @@ df_calls <- read.csv("911BehavioralHealthDiversion.csv")
 pd_names <-c("Central","Eastern","Northeastern","Northern","Northwestern","Southeastern","Southern","Southwestern","Western")
 time_y <- 2021:2024
 time_m <- 1:12
-df0 <- data.frame(month=rep(time_m, times=length(pd_names)*length(time_y)),
-		    year=rep(time_y, times=length(pd_names), each=length(time_m)),
-		    policeDistrict=rep(pd_names, each=length(time_m)*length(time_y))) %>%
+df0 <- data.frame(policeDistrict=rep(pd_names, times=length(time_m)*length(time_y)),
+		    year=rep(time_y, times=length(time_m), each=length(pd_names)),
+		    month=rep(time_m, each=length(pd_names)*length(time_y))) %>%
 	filter((year > 2021 | month >= 6) & (year < 2024 | month <= 1))
 #attatch call data
 df1 <- df_calls %>%
@@ -17,7 +17,7 @@ df1 <- df_calls %>%
 		month=as.numeric(substring(callDateTime,6,7))) %>%
 	group_by(month,year,policeDistrict) %>%
 	summarise(callFreq=n())
-df2 <- merge(df0, df1, all.x=TRUE)
+df2 <- merge(df0, df1,all.x=TRUE)
 df3 <- df2 %>%
 	mutate(callFreq=ifelse(is.na(callFreq),0,callFreq))
 df3
