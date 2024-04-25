1b8a84c63ad2194e6b1f4282638b75ce705338b6
vsrivastava <vai.sriv@icloud.com>
2024-04-25 12:01:16 -0400

    start work on parks (still need to fix multi-district)

--- data/CleanData.r
+++ data/CleanData.r
@@ -1,24 +1,58 @@
+setwd("data")
 library("dplyr")
-df_calls <- read.csv("911BehavioralHealthDiversion.csv")
-#Entries are identified by police district, year, and month
-#for each entry we observe call frequency, number of trees, and whether the month is peak fly activity
 
-#identifying variables
-pd_names <-c("Central","Eastern","Northeastern","Northern","Northwestern","Southeastern","Southern","Southwestern","Western")
+# Entries are identified by police district, year, and month
+# for each entry we observe call frequency, number of trees,
+# and whether the month is peak fly activity
+
+# identifying variables
+pd_names <- c(
+  "Central",
+  "Eastern",
+  "Northeastern",
+  "Northern",
+  "Northwestern",
+  "Southeastern",
+  "Southern",
+  "Southwestern",
+  "Western"
+)
 time_y <- 2021:2024
 time_m <- 1:12
-df0 <- data.frame(policeDistrict=rep(pd_names, times=length(time_m)*length(time_y)),
-		    year=rep(time_y, times=length(time_m), each=length(pd_names)),
-		    month=rep(time_m, each=length(pd_names)*length(time_y))) %>%
-	filter((year > 2021 | month >= 6) & (year < 2024 | month <= 1))
-#attatch call data
-df1 <- df_calls %>%
-	mutate(year=as.numeric(substring(callDateTime,1,4)),
-		month=as.numeric(substring(callDateTime,6,7))) %>%
-	group_by(month,year,policeDistrict) %>%
-	summarise(callFreq=n())
-df2 <- merge(df0, df1,all.x=TRUE)
-df3 <- df2 %>%
-	mutate(callFreq=ifelse(is.na(callFreq),0,callFreq))
-df3
-write.csv(df3,"callFrequency.csv")
+
+df_calls0 <- read.csv("911BehavioralHealthDiversion.csv")
+
+# attach call data
+df_calls1 <- df_calls %>%
+  mutate(
+    year = as.numeric(substring(callDateTime, 1, 4)),
+    month = as.numeric(substring(callDateTime, 6, 7))
+  ) %>%
+  group_by(month, year, policeDistrict) %>%
+  summarise(callFreq = n())
+
+df_call_freq0 <- data.frame(
+  policeDistrict = rep(pd_names, times = length(time_m) * length(time_y)),
+  year = rep(time_y, times = length(time_m), each = length(pd_names)),
+  month = rep(time_m, each = length(pd_names) * length(time_y))
+) %>%
+  filter((year > 2021 | month >= 6) & (year < 2024 | month <= 1))
+
+df_call_freq1 <- merge(df_callFreq0, df_calls1, all.x = TRUE)
+df_call_freq2 <- df_call_freq1 %>%
+  mutate(callFreq = ifelse(is.na(callFreq), 0, callFreq))
+
+df_call_freq <- df_call_freq2
+
+df_parks0 <- read.csv("Parks.csv")
+
+df_parks1 <- df_parks0 %>%
+  mutate(policeDistrict = policeDstrct) %>%
+  group_by(policeDistrict) %>%
+  tally()
+
+# TODO: split the strings here
+df_parks2 <- df_parks1 %>%
+  mutate()
+
+# write.csv(df_call_freq, "callFrequency.csv")
