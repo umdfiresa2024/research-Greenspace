0f939f65288edaa534f8cf76c81bb64c47cf1d88
Thanicha Ruangmas <106334045+ruangmas@users.noreply.github.com>
2024-09-30 13:10:54 -0400

    upload terra extracted data

---
+++ query-ecostress.R
@@ -0,0 +1,31 @@
+
+library(httr)
+#read list of links to download from a text file
+files<-read.table("G:/Shared drives/2024 FIRE-SA/DATA/ECOSTRESS/files.txt")
+files<-files$V1
+
+#set folder to store the downloaded files
+dl_dir <- "G:/Shared drives/2024 FIRE-SA/DATA/ECOSTRESS/"                                 
+setwd(dl_dir)                                                
+
+i<-1
+files[i]
+
+#for (i in 1:length(files)) {
+for (i in 1:length(50)) {
+  
+  #make the downloaded filename an abbreviated version of the original file name
+  filename <-  tail(strsplit(files[i], "ECOSTRESS_L2_LSTE_")[[1]], n = 1) 
+  
+  # write file to disk 
+  response <- GET(files[i], write_disk(filename, overwrite = TRUE), progress(),
+                  authenticate("truangmas1812", "FIRESAR00m#2207"), set_cookies("LC" = "cookies"))
+  
+  # Check to see if file downloaded correctly
+  if (response$status_code == 200) {
+    print(sprintf("%s downloaded at %s", filename, dl_dir))
+  } else {
+    print(sprintf("%s not downloaded. Verify that your username and password are correct in %s", filename, netrc))
+  }
+}
+
--- query-merra.R
+++ query-merra.R
@@ -1,12 +1,11 @@
 
 library(httr)
-
 #read list of links to download from a text file
-files<-read.table("G:/My Drive/2023 FIRE-SA/Lectures/2023 FIRE298 Week 4/merra-filelinks.txt")
+files<-read.table("G:/Shared drives/2024 FIRE-SA/DATA/TERRA/terra-files.txt")
 files<-files$V1
 
 #set folder to store the downloaded files
-dl_dir <- "G:/My Drive/2023 FIRE-SA/Lectures/2023 FIRE298 Week 4/merra/"                                 
+dl_dir <- "G:/Shared drives/2024 FIRE-SA/DATA/TERRA/"                                 
 setwd(dl_dir)                                                
 
 i<-1
@@ -15,11 +14,11 @@ files[i]
 for (i in 1:length(files)) {
   
   #make the downloaded filename an abbreviated version of the original file name
-  filename <-  tail(strsplit(files[i], "tavgM_2d_flx_Nx.")[[1]], n = 1) 
+  filename <-  tail(strsplit(files[i], ".h12v05.061.")[[1]], n = 1) 
   
   # write file to disk 
   response <- GET(files[i], write_disk(filename, overwrite = TRUE), progress(),
-                  authenticate("truangmas1812", "VulcanRoom#2207"), set_cookies("LC" = "cookies"))
+                  authenticate("truangmas1812", "FIRESAR00m#2207"), set_cookies("LC" = "cookies"))
   
   # Check to see if file downloaded correctly
   if (response$status_code == 200) {
