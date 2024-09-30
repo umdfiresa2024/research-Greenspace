81120414a96a43a1fe38cd4a96ee9ba6dcb860b8
ruangmas <ruangmas@umd.edu>
2024-09-30 15:19:51 -0400

    make panel

--- toFindData.R
+++ toFindData.R
@@ -5,14 +5,57 @@ days<-read_table("terra-files.txt", col_names = FALSE)
 
 #X is the column with the full file URL
 day2<-days %>%
-  mutate(actual_date= substr(X1, 87, 93)) |>
+  mutate(actual_date= substr(X1, 84, 90)) |>
   mutate(filename=substr(X1, 145, 164))
 
-write.csv(day2, "datesTerra.csv", row.names = FALSE)
+lst<- read.csv("terra_lst.csv") 
 
-lst<- read.csv("terra_lst.csv") |>
-  filter(!is.na(LST_Day_1km))
+library("terra")
+pdshape<-vect("Police_Districts_2023/Police_Districts_2023.shp")
+pdf<-as.data.frame(pdshape) |>
+  select(OBJECTID, Dist_Name) |>
+  rename(ID=OBJECTID)
 
-df<- merge(day2, lst, by="filename") 
+lst2<-merge(lst, pdf, by="ID")
 
-olddata<-read.csv("data/callFrequency.csv")
+df<- merge(day2, lst2, by="filename") |>
+  select(LST_Day_1km, LST_Night_1km, actual_date, Dist_Name) |>
+  mutate(year=as.numeric(substr(actual_date, 1, 4))) |>
+  mutate(doy=as.numeric(substr(actual_date, 5, 7))) 
+
+df21<-df |>
+  filter(year==2021) |>
+  mutate(date=as.Date(doy, origin="2021-01-01"))
+
+df22<-df |>
+  filter(year==2022) |>
+  mutate(date=as.Date(doy, origin="2022-01-01"))
+
+df23<-df |>
+  filter(year==2023) |>
+  mutate(date=as.Date(doy, origin="2023-01-01"))
+
+dfall<-rbind(df21, df22, df23)
+
+df_long<-pivot_longer(dfall, cols=1:2, names_to="daytime", values_to = "temp_K") |>
+  filter(!is.na(temp_K)) |>
+  mutate(daytime=ifelse(daytime=="LST_Day_1km", 1, 0)) |>
+  rename(policeDistrict=Dist_Name)
+
+######aggregate calls by date, day, night##########################################
+
+calls<-read.csv("data/911BehavioralHealthDiversion.csv") |>
+  mutate(time=as.numeric(substr(callDateTime, 12, 13))) |>
+  mutate(date=as.Date(substr(callDateTime, 1, 10), format="%Y/%m/%d")) |>
+  mutate(daytime=ifelse(time>=6 & time<=18,1,0)) |>
+  group_by(policeDistrict, date, daytime) |>
+  tally() |>
+  rename(callscount=n)
+
+#########merge data#############################################################
+
+dfm<-merge(df_long, calls, by=c("daytime", "policeDistrict", "date"), all.x=TRUE) |>
+  mutate(callscount=ifelse(is.na(callscount),0, callscount)) |>
+  mutate(temp_F=(temp_K-273.15)*1.8+32)
+
+write.csv(dfm, "panel.csv", row.names = F)
