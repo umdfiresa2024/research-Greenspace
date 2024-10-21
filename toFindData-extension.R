fbf92b8194345531ea51de062c373b4b59a7851f
sadorrab <ben.a.sadorra@gmail.com>
2024-10-21 10:16:04 -0400

    add new variables: pollution

---
+++ toFindData-extension.R
@@ -0,0 +1,22 @@
+library("tidyverse")
+panel<-read.csv("panel.csv")
+
+###Add PM2.5 ### {{{
+
+pm21<-read.csv("data/EPA/pm25_2021.csv")
+pm22<-read.csv("data/EPA/pm25_2022.csv")
+pm23<-read.csv("data/EPA/pm25_2023.csv")
+
+pollution <- rbind(pm21, pm22, pm23) %>%
+    mutate(date=sapply(mdy(Date), function(d) sprintf("%s", d))) %>% # trust
+    select(date, Daily.Mean.PM2.5.Concentration) %>%
+    rename("Daily.Mean.PM25.ug/m3.LC" = "Daily.Mean.PM2.5.Concentration")
+# TODO: handle NA, because pollution is not reported sometimes for some reason
+panel_ext<-left_join(x=panel, y=pollution, by="date")
+### }}}
+
+### TODO: Add Weather ###
+
+### TODO: Add Green Coverage ###
+
+write.csv(panel_ext, "panel_ext.csv", row.names=FALSE)
