328a34c9e90b6f66805ce65a7f0d9d6f4593a3e6
ruangmas <ruangmas@umd.edu>
2024-11-08 13:34:44 -0500

    update era5 crop

--- crop_era5.R
+++ crop_era5.R
@@ -16,18 +16,12 @@ crs(utci)<-"WGS84"
 pd <- vect("Police_Districts_2023/Police_Districts_2023.shp")
 pd_proj<-project(pd, crs(utci))
 
-ext(pd_proj)
-
 e<-ext(c(-77, -76, 39.1, 39.4))
 p <- as.polygons(e, crs="+proj=longlat")
-
-d <- terra::crop(x = utci, y=p, snap="in")
 
 plot(e)
 plot(d[[2]], add=TRUE)
 plot(pd_proj, add=TRUE)
-
-
 
 d <- terra::crop(x = utci, y=pd_proj, snap="in", mask=TRUE)
 
